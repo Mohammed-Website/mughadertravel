3bc697796f61d093014c98b1ba47feccc4793d85
Bandar-Zuhair <my.github.websites@gmail.com>
2024-12-03 21:56:54 +0700

    .

--- mughader-travel.js
+++ mughader-travel.js
@@ -607,17 +607,3 @@ openWhatsAppNumber = function () {
     let url = `https://wa.me/${whatsappNumber}?text=${message}`;
     window.open(url, "_blank"); // Opens in a new tab
 }
-
-
-
-// Create and append script for 'Ionicons' Website Icons (Module Script)
-let ioniconsModuleScript = document.createElement('script');
-ioniconsModuleScript.type = 'module';
-ioniconsModuleScript.src = 'https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js';
-document.body.appendChild(ioniconsModuleScript);
-
-// Create and append script for 'Ionicons' Website Icons (Module Script)
-let ioniconsNomoduleScript = document.createElement('script');
-ioniconsNomoduleScript.setAttribute('nomodule', '');
-ioniconsNomoduleScript.src = 'https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js';
-document.body.appendChild(ioniconsNomoduleScript);
